e64256d2a59863bd6080812d02bcb5d86fade7bd
Maksim-Salnikov <salomaksim123@gmail.com>
2023-12-15 15:20:39 +0300

    add router content

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  return(
-    <div className="wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+  return (
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Dialogs" element={<Dialogs />} />
+          <Route path="/News" element={<News />} />
+          <Route path="/Music" element={<Music />} />
+          <Route path="/Settings" element={<Settings />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
-export default App
+export default App;
--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import "./Navbar.css";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="navbar-list-item">
-          <a href="#" className="navbar-list-item__link">
+          <a href="/Profile" className="navbar-list-item__link">
             Profile
           </a>
         </li>
         <li className="navbar-list-item">
-          <a href="#" className="navbar-list-item__link">
+          <a href="/Dialogs" className="navbar-list-item__link">
             Messages
           </a>
         </li>
         <li className="navbar-list-item">
-          <a href="#" className="navbar-list-item__link">
+          <a href="News" className="navbar-list-item__link">
             News
           </a>
         </li>
         <li className="navbar-list-item">
-          <a href="#" className="navbar-list-item__link">
+          <a href="/Music" className="navbar-list-item__link">
             Music
           </a>
         </li>
         <li className="navbar-list-item">
-          <a href="#" className="navbar-list-item__link">
+          <a href="/Settings" className="navbar-list-item__link">
             Settings
           </a>
         </li>
