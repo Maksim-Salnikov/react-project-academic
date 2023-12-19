0e27a05f2b1aea3bdbc108e6db0d375cc6b12d51
Maksim-Salnikov <salomaksim123@gmail.com>
2023-12-19 16:31:02 +0300

    homework leasson 29 - part 1 - styles messages

--- app/src/components/Dialogs/Message/Message.jsx
+++ app/src/components/Dialogs/Message/Message.jsx
@@ -3,7 +3,15 @@ import "./Message.css";
 
 const Message = (props) => {
   return (
-    <div className="dialogs-correspondense-message">{props.textMessage}</div>
+    <div className="dialogs-correspondense-message">
+      <img
+        className="dialogs-correspondense-message__avatar"
+        src="https://i.gifer.com/origin/5b/5b93fc5b280e1231907822faf9608a43_w200.gif"
+      />
+      <span className="dialogs-correspondense-message__text">
+        {props.textMessage}
+      </span>
+    </div>
   );
 };
 
