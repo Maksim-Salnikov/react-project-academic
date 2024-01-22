a2d5ec04ad35e255d6c778dd9b70289578155f84
Maksim-Salnikov <salomaksim123@gmail.com>
2024-01-22 16:24:10 +0300

    leasson 53: add first class component - Uses

--- app/src/components/Users/Users.jsx
+++ app/src/components/Users/Users.jsx
@@ -3,38 +3,42 @@ import User from "./User/User";
 import styles from "./Users.module.css";
 import axios from "axios";
 
-const Users = (props) => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
-        props.setUsers(response.data.items);
+        this.props.setUsers(response.data.items);
       });
   }
 
-  let usersElements = props.users.map((user) => {
+  render() {
     return (
-      <User
-        follow={props.follow}
-        unfollow={props.unfollow}
-        id={user.id}
-        avatar={user.photos.small}
-        followed={user.followed}
-        name={user.name}
-        status={user.status}
-        // city={user.location.city}
-        // country={user.location.country}
-        key={user.id}
-      />
+      <div className={styles.wrapper}>
+        <h3 className={styles.title}>Users</h3>
+        <div className={styles.usersWrapper}>
+          {this.props.users.map((user) => {
+            return (
+              <User
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+                id={user.id}
+                avatar={user.photos.small}
+                followed={user.followed}
+                name={user.name}
+                status={user.status}
+                // city={user.location.city}
+                // country={user.location.country}
+                key={user.id}
+              />
+            );
+          })}
+        </div>
+        <button className={styles.button}>Show more</button>
+      </div>
     );
-  });
-  return (
-    <div className={styles.wrapper}>
-      <h3 className={styles.title}>Users</h3>
-      <div className={styles.usersWrapper}>{usersElements}</div>
-      <button className={styles.button}>Show more</button>
-    </div>
-  );
-};
+  }
+}
 
 export default Users;
