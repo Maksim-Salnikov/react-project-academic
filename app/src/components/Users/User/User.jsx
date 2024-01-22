daf227037f6acb2de06a8d6869d0e90f5fe1cb3a
Maksim-Salnikov <salomaksim123@gmail.com>
2024-01-22 15:19:39 +0300

    leasson 51: add API via axioc library

--- app/src/components/Users/User/User.jsx
+++ app/src/components/Users/User/User.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "./User.module.css";
+import defaultAvatar from "../../../assets/images/defaultAvatar.png";
 
 const User = (props) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.leftside}>
-        <img src={props.avatar} alt="аватар" className={styles.avatar} />
+        <img
+          src={props.avatar != null ? props.avatar : defaultAvatar}
+          alt="аватар"
+          className={styles.avatar}
+        />
         <div>
           {props.followed ? (
             <button
               className={styles.button}
               onClick={() => {
-                props.unfollow(props.number);
+                props.unfollow(props.id);
               }}
             >
               Unfollow
@@ -20,7 +25,7 @@ const User = (props) => {
             <button
               className={styles.button}
               onClick={() => {
-                props.follow(props.number);
+                props.follow(props.id);
               }}
             >
               Follow
@@ -30,12 +35,12 @@ const User = (props) => {
       </div>
       <div className={styles.rightside}>
         <div className={styles.leftsideWrapper}>
-          <span className={styles.fullname}>{props.fullname}</span>
+          <span className={styles.fullname}>{props.name}</span>
           <span className={styles.status}>{props.status}</span>
         </div>
         <div className={styles.rightsideWrapper}>
-          <span className={styles.country}>{props.country}</span>
-          <span className={styles.city}>{props.city}</span>
+          <span className={styles.country}>{"props.country"}</span>
+          <span className={styles.city}>{"props.city"}</span>
         </div>
       </div>
     </div>
--- app/src/components/Users/Users.jsx
+++ app/src/components/Users/Users.jsx
@@ -1,47 +1,15 @@
 import React from "react";
 import User from "./User/User";
 import styles from "./Users.module.css";
+import axios from "axios";
 
 const Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        avatar:
-          "https://sun9-62.userapi.com/impg/LHcdw0H9Ah5M3VtmxyazUzgmypJcF078OrZHuQ/jxE8K-tM3as.jpg?size=1620x2160&quality=95&sign=40f55c8d3d7d989590efe8dd8db4d7d7&type=album",
-        followed: true,
-        fullname: "Gleb K.",
-        status: "I suck dick",
-        location: { city: "Saint-Pitersburg", country: "Russia" },
-      },
-      {
-        id: 2,
-        avatar:
-          "https://sun9-20.userapi.com/impf/DVI3OcdZ9LtwDGrMOxPaLHevZOXZSKNpLAdfAw/L8VyMnZXPk0.jpg?size=2560x1707&quality=95&sign=295665408521f78d9f90f95f2c486568&type=album",
-        followed: false,
-        fullname: "Sasha S.",
-        status: "I suck dick too",
-        location: { city: "Saint-Pitersburg", country: "Russia" },
-      },
-      {
-        id: 3,
-        avatar:
-          "https://sun9-38.userapi.com/impf/IO18n7eXlOZsuC0trp2Zm5FDDW5CJUlci20ExA/Ydp_zJ_JkYw.jpg?size=1266x1688&quality=96&sign=a6b0c9f99db65f0a440b410afc86b2a6&type=album",
-        followed: true,
-        fullname: "Ilya A.",
-        status: "I don't suck dick",
-        location: { city: "Lipetsk", country: "Russia" },
-      },
-      {
-        id: 4,
-        avatar:
-          "https://sun9-77.userapi.com/impg/i1FymPsZRec-Bc8tRN3BNaS05XCdMTBhmcS-fg/4Y_RKlCTohA.jpg?size=2560x1707&quality=95&sign=a4d8136a0421b85798738fe78467f358&type=album",
-        followed: true,
-        fullname: "Vladislav D.",
-        status: "I don't suck dick too",
-        location: { city: "Lipetsk", country: "Russia" },
-      },
-    ]);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
   }
 
   let usersElements = props.users.map((user) => {
@@ -49,18 +17,17 @@ const Users = (props) => {
       <User
         follow={props.follow}
         unfollow={props.unfollow}
-        number={user.id}
-        avatar={user.avatar}
+        id={user.id}
+        avatar={user.photos.small}
         followed={user.followed}
-        fullname={user.fullname}
+        name={user.name}
         status={user.status}
-        city={user.location.city}
-        country={user.location.country}
+        // city={user.location.city}
+        // country={user.location.country}
         key={user.id}
       />
     );
   });
-
   return (
     <div className={styles.wrapper}>
       <h3 className={styles.title}>Users</h3>
