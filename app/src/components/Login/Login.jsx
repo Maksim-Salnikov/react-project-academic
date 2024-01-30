8c15e40da0fe3bfc0bf5cedefc443dde4276cc62
Maksim-Salnikov <salomaksim123@gmail.com>
2024-01-30 23:48:16 +0300

    leasson 75: add redux-form

--- app/src/components/Login/Login.jsx
+++ app/src/components/Login/Login.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import styles from "./Login.module.css";
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  console.log("RERENDER");
+  return (
+    <form className={styles.form} onSubmit={props.handleSubmit}>
+      <Field
+        component={"input"}
+        name={"Login"}
+        placeholder="Логин"
+        className={styles.login}
+      />
+      <Field
+        component={"input"}
+        name={"Password"}
+        placeholder="Пароль"
+        className={styles.password}
+      />
+      <div>
+        <Field
+          component={"input"}
+          name={"rememberMe"}
+          type="checkbox"
+          className={styles.checkbox}
+        />
+        <span className={styles.remember}>Запонмить меня</span>
+      </div>
+      <button className={styles.button}>Войти</button>
+    </form>
+  );
+};
+
+const LoginReduxForm = reduxForm({
+  form: "login",
+})(LoginForm);
 
 const Login = () => {
-  return <h3>Login</h3>;
+  let onSubmit = (passData) => {
+    console.log(passData);
+  };
+  return (
+    <div>
+      <h1 className={styles.title}>Войти</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Login;
